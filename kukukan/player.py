bc275d2320743684300691b5b976f7ea2057d333
kukufuckingkan <keitakukukhan@gmail.com>
2024-09-20 20:16:16 +0000

    Create player.py

---
+++ kukukan/player.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 17:47:16 2024
+
+@author: kukukan
+"""
+
+
+from IPython.core.display import HTML
+from IPython.display import HTML
+
+
+def player(filePath: str, env: str) -> str:
+    path = getPath(filePath, env)
+
+    # HTML for the audio player without newlines
+    audio_html = '''<audio controls style="width:150px">
+                      <source src="{}" type="audio/mpeg">
+                      Your browser does not support the audio element.
+                    </audio>'''.format(path)
+    
+    return audio_html
+
+
+def getPath(path:str,env: str) -> str:
+    PATH_RESOURCE = 'resource/'
+    
+    if PATH_RESOURCE in path:
+        if 'DEV' == env:
+            return path.replace(PATH_RESOURCE,'')
+        return path
+    return ''
+
+    
