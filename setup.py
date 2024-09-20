00ba203212091431304489268e91497c2eda880f
kukufuckingkan <keitakukukhan@gmail.com>
2024-09-20 20:10:51 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="kukufuckingkan",
+    version="0.6",
+    packages=["audio"],
+    install_requires=[
+        # List your dependencies here
+        "requests",
+        "flask",
+    ],
+)
