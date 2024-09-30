6e2902a74faad839b6d300efc30284988a21db0f
kukufuckingkan <keitakukukhan@gmail.com>
2024-09-30 23:29:22 +0000

    ana version

--- memory/database.py
+++ memory/database.py
@@ -2,31 +2,62 @@
 # python3 -m venv venv 
 # source venv/bin/activate
 
-from sqlalchemy import create_engine, Engine , Connection
+from sqlalchemy import create_engine, Engine ,Table, Column, Integer, String, MetaData, Index,Text,BINARY
 from pandas import DataFrame,ExcelFile
 import logging as log
-from typing import Dict
 
 class Sqlite:
     def __init__(self,folder: str, subfolder:str) -> None:
         url = 'sqlite:///{folder}_{subfolder}_ana.db'.format(folder= folder,subfolder= subfolder)
         database = create_engine(url)
-        self.__connection = database.connect()
+        metadata = MetaData()
+        metadata.create_all(database)
+        self.__database = database
 
-    def connection(self) -> Connection:
-        return self.__connection
-    
-        
-    def initAll(self,book: ExcelFile, sheet: str)-> int:
+
+    def createTable(sheetName: str, database: Engine):
+        metadata = MetaData()
+        if 'word'.__eq__(sheetName):
+            Table(
+                sheetName, metadata,
+                Column('id', Integer, primary_key=True),  
+                Column('text', String),
+                Column('meaning', Text),
+                Column('voice', BINARY),
+                Column('root', Integer),
+                Column('type', Integer),
+                Column('version', Integer),
+                Index('ix_users_text', 'text'),
+                Index('ix_users_root', 'root')                                                
+            )
+            
+            metadata.create_all(database)
+
+        elif 'image'.__eq__(sheetName):
+            Table(
+                sheetName, metadata,
+                Column('id', Integer, primary_key=True),
+                Column('data', BINARY),
+                Column('name_id', Integer),
+                Index('ix_image_name_id', 'name_id'),                                              
+            )
+
+            metadata.create_all(database)   
+        else:
+            pass               
+
+            
+    def initFromExcell(self,book: ExcelFile, sheet: str)-> int:
+        connection = self.__database.connect()
         df = book.parse(sheet_name= sheet)
-        db = self.populate(tableName= sheet,dataframe=df)
-        return db        
+        rowCount = df.to_sql(name=sheet,con= connection, if_exists= 'append') 
+
+        log.info(f'number of affecred rows: {rowCount}')                  
+        return rowCount        
  
                     
-    def initFromFrame(self, frame : DataFrame , table: str,dataType: Dict, indexLable: str)-> int:
-        
-        rowCount = frame.to_sql(name=table,con=self.__connection, if_exists= 'append',dtype=dataType,index_label=indexLable)
+    def initFromFrame(self, frame : DataFrame , table: str)-> int:
+        connection = self.__database.connect()
+        rowCount = frame.to_sql(name=table,con= connection, if_exists= 'append')
         log.info(f'number of affecred rows: {rowCount}')          
         return rowCount
-
-        
